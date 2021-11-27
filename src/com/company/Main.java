22b28f222bc871c181e22485866b42cb0778fca9
hanafiaustin <muhammadhanafi009@gmail.com>
2021-11-27 06:06:03 +0000

    first commit

---
+++ src/com/company/Main.java
@@ -0,0 +1,15 @@
+package com.company;
+
+public class Main {
+    public static void main(String[] args) {
+
+     int a = 0;
+     int hasil;
+     do {
+         hasil = 10 + a;
+         a++;
+         System.out.println(hasil);
+     }while (a <= 10);
+
+    }
+}
